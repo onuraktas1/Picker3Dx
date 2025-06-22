2da20bbeab073d661db2885f254cade54e664d33
Onur Aktas <onuraktas0640@gmail.com>
2025-06-22 17:55:56 +0300

    CameraManager

--- Assets/Scripts/RunTime/Managers/CameraManager.cs
+++ Assets/Scripts/RunTime/Managers/CameraManager.cs
@@ -1,9 +1,57 @@
+using Cinemachine;
+using RunTime.Signals;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace RunTime.Managers
 {
     public class CameraManager : MonoBehaviour
     {
-        
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+        private float3 _firstPosition;
+
+        private void Start()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _firstPosition = transform.position;
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CameraSignals.Instance.onSetCameraTarget += OnSetCameraTarget;
+            CoreGameSignals.Instance.onReset += OnReset;
+        }
+
+        private void OnReset()
+        {
+            transform.position = _firstPosition;
+        }
+
+        private void OnSetCameraTarget()
+        {
+            // var player = FindObjectOfType<PlayerManager>().transform;
+            // virtualCamera.Follow = player;
+            // virtualCamera.LookAt = player;
+        }
+
+        private void UnSubscribeEvents()
+        {
+            CameraSignals.Instance.onSetCameraTarget -= OnSetCameraTarget;
+            CoreGameSignals.Instance.onReset -= OnReset;
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
     }
 }
